0c1f5ba6e8739eb51dbb215c31cc384899a344e5
Braeden57 <sowinskibraeden@gmail.com>
2020-10-22 11:05:44 -0700

    'Update'

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,9 @@ const server = require('http').Server(app)
 
 module.exports = function (app, server) {
     app.get('/', function (req, res) {
-        res.render('index')
+        res.render('index');
+    });
+    app.get('/images', function (req, res) {
+        res.render('gallary');
     });
 }
